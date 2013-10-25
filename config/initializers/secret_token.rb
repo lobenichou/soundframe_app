d15c4ccfa64c02d0b4c8b8afacf109ec716d5a6e
laurenbenichou <laurenbenichou@gmail.com>
2013-10-24 22:05:12 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoundframeApp::Application.config.secret_token = '91fc02b999307b6e3b89844a53926afbaa605b60dcd10c391ba27c3f9fd0c67574059e052a34d4e7557e7ebbcf470c7148cf21637846231ec852909f0d575e41'
