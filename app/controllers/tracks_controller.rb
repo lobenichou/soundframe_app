346dea38fe8c2101d39e536d31733c1a0439b82f
Jacqueline Herrlin <j.m.herrlin@gmail.com>
2013-10-27 20:21:54 -0700

    refactor tracks controller

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -30,56 +30,67 @@ class TracksController < ApplicationController
 	end 
 		
 	def create
-			response = []
-		 	@query = params[:location]
+	 	query = params[:location]
 
-		 	@query.each do |item, location|
-		 			location[:input].gsub!(" ", '+')
-		 			location[:input].gsub!(",", "+")
-					request = Typhoeus.get("http://maps.googleapis.com/maps/api/geocode/json?address=#{location[:input]}&sensor=true")
-					result_hash = JSON.parse(request.body)
-					response << result_hash
-			end
+	 	response = api_call(query)
 
-
-# binding.pry
-			count = 0
-			response.each do |hash|
-				unless hash["results"] == []
-					@lat = hash["results"][0]["geometry"]["location"]["lat"]
-					@lng = hash["results"][0]["geometry"]["location"]["lng"]
-					# binding.pry
-
-					track = Track.find_or_create_by_soundcloud_track_id(@query[count.to_s]["track_id"])
-
-					track.latitude =  @lat
-					track.longitude = @lng
-					track.save
-					
-				end
-				count += 1
-			end
-
-			gon.coordinates = {}
-			gon.permalink_url = {}
-			gon.track_title = {}
-			gon.track_image = {}
-
-			@all_tracks = Track.all
-
-				@all_tracks.each do |track|
-					unless track.latitude == nil
-					gon.coordinates[track.id] = [track.latitude, track.longitude]
-					gon.permalink_url[track.id] = track.permalink_url
-					gon.track_title[track.id] = track.title
-					gon.track_image[track.id] = track.artwork_url
-					end
-
-				end
-
-			render :map
+		count = 0
+		response.each do |hash|
+			create_track(hash, count, query)
+			count += 1
 		end
-		
+
+		setup_map
+
+		render :map
+	end
+
+	private 
+
+	def api_call(query)
+		results = []
+		query.each do |item, location|
+ 			location[:input].gsub!(" ", '+')
+ 			location[:input].gsub!(",", "+")
+			request = Typhoeus.get("http://maps.googleapis.com/maps/api/geocode/json?address=#{location[:input]}&sensor=true")
+			result_hash = JSON.parse(request.body)
+			results << result_hash
+		end
+		results
+	end
+
+	def create_track(hash, count, query)
+		unless hash["results"] == []
+			@lat = hash["results"][0]["geometry"]["location"]["lat"]
+			@lng = hash["results"][0]["geometry"]["location"]["lng"]
+			
+			track = Track.find_or_create_by_soundcloud_track_id(query[count.to_s]["track_id"])
+
+			track.latitude =  @lat
+			track.longitude = @lng
+			track.save
+		end
+	end
+
+	def setup_map
+		gon.coordinates = {}
+		gon.permalink_url = {}
+		gon.track_title = {}
+		gon.track_image = {}
+
+		all_tracks = Track.all
+
+		all_tracks.each do |track|
+			unless track.latitude == nil
+				gon.coordinates[track.id] = [track.latitude, track.longitude]
+				gon.permalink_url[track.id] = track.permalink_url
+				gon.track_title[track.id] = track.title
+				gon.track_image[track.id] = track.artwork_url
+			end
+
+		end
+	end
+
 end
 
 
