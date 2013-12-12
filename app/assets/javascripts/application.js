9066a6ce1a940ac38beb4c0b20baf1ca489e7397
laurenbenichou <laurenbenichou@gmail.com>
2013-12-11 16:14:51 -0800

    foundation installed and home page modified

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require foundation
 //= require_tree .
+
+
+$(document).foundation();
+$(function(){ $(document).foundation(); });
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :soundcloud_client
+  before_filter :soundcloud_client, :gon_variable
+
   include SessionsHelper
 
   def soundcloud_client
   	@client = Soundcloud.new(:client_id => ENV['SOUNDCLOUD_CLIENT_ID'])
+  end
+
+  def gon_variable
+    gon.client_id = ENV['SOUNDCLOUD_CLIENT_ID']
+    gon.cloud_made_api_key = ENV['ClOUD_MADE_API_KEY']
   end
 
 end
