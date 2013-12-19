735eb3ee70f0fa991b3eb6ec0e6279541f0b278a
laurenbenichou <laurenbenichou@gmail.com>
2013-12-19 00:02:30 -0800

    removed ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ SoundframeApp::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  # config.force_ssl = true
+  config.force_ssl = false
 end
